7a8fe63e4c13504231b1d06a357b3f1873f78ae4
Yusuf Enes Aras <y.enesaras@hotmail.com>
2021-09-14 16:48:33 +0300

    16. ders 14.09.2021

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal
+    {
+        List<Category> ListAllCategory();
+        void AddCategory(Category category);
+        void DeleteCategort(Category category);
+        void UpdateCategort(Category category);
+        Category GetById(int id);
+
+    }
+}
---
+++ DataAccessLayer/Repositories/CategoryRepository.cs
@@ -0,0 +1,47 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        public void AddCategory(Category category)
+        {
+            using var c = new Context();
+            c.Add(category);
+            c.SaveChanges();
+        }
+
+        public void DeleteCategort(Category category)
+        {
+            using var c = new Context();
+            c.Remove(category);
+            c.SaveChanges();
+        }
+
+        public Category GetById(int id)
+        {
+            using var c = new Context();
+            return c.Categories.Find(id);
+        }
+
+        public List<Category> ListAllCategory()
+        {
+            using var c = new Context();
+            return c.Categories.ToList();
+        }
+
+        public void UpdateCategort(Category category)
+        {
+            using var c = new Context();
+            c.Update(category);
+            c.SaveChanges();
+        }
+    }
+}
